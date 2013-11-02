32cf2379a857f89a89ca1ed8aa3ce74480efa20e
pbo <github@paulbouzakis.com>
2013-11-02 15:45:55 -0400

    Kill jquery dependency.

--- component.js
+++ component.js
@@ -1,6 +1,5 @@
 "use strict";
 
-var $ = require("jquery-browserify");
 var View = require("./View");
 var makeEmitter = require("pubit-as-promised").makeEmitter;
 
@@ -19,15 +18,16 @@ module.exports = function mixinComponent(target, template) {
 
     target.mixins = function (mixin) {
         // All components come w/ showable mixin (Only supported mixin via option)
-        // Next step is that mixins can be an array
-        if (mixin === "showable") {
-            target.show = function () {
-                $(target.element).show();
-            };
+        // Next step is that mixins can be an arra
+        function setDisplay(display) {
+            if (target.element) {
+                target.element.style.display = display;
+            }
+        }
 
-            target.hide = function () {
-                $(target.element).hide();
-            };
+        if (mixin === "showable") {
+            target.show = setDisplay.bind(target, "block"); // Assume we are show block elements.
+            target.hide = setDisplay.bind(target, "none");
         }
 
         return target;
